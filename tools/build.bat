0683d5ef3426824e6b7a42c5351a567188a3014b
KiG Organization <misteranthems5@gmail.com>
2025-11-28 21:54:59 +0500

    Update build.bat

--- tools/build.bat
+++ tools/build.bat
@@ -1,21 +1,20 @@
-/*
- * This file is part of the QuarkBIOS project.
- *
- * Copyright (C) 2025 KiG Organization
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+@echo off
+rem This file is part of the QuarkBIOS project.
+rem
+rem Copyright (C) 2025 KiG Organization
+rem
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU Affero General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU Affero General Public License for more details.
+rem
+rem You should have received a copy of the GNU Affero General Public License
+rem along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 @echo off
 REM Build script for QuarkBIOS
@@ -30,3 +29,4 @@ if %errorlevel% neq 0 (
 
 
 echo Build successful! Output: ..\build\boot.bin
+
